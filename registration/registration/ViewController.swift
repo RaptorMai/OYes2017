a79881e6e3e7c03f0d1d04f5d5935f4e86c814c5
Sulagshan <Sulagshan@gmail.com>
2017-07-07 21:20:58 -0400

    Working phone authentication
    First commit with working phone authetication
    
    UI imcompleted

---
+++ registration/registration/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  registration
+//
+//  Created by devuser on 2017-07-07.
+//  Copyright © 2017 devuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
