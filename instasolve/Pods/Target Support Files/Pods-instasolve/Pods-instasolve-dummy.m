14bef0c19864e0a6f166888fa804ee2831cc0d42
Sulagshan <Sulagshan@gmail.com>
2017-07-12 15:45:08 -0400

    created instasolve

---
+++ instasolve/Pods/Target Support Files/Pods-instasolve/Pods-instasolve-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_instasolve : NSObject
+@end
+@implementation PodsDummy_Pods_instasolve
+@end
---
+++ instasolve/instasolve/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instasolve
+//
+//  Created by devuser on 2017-07-12.
+//  Copyright © 2017 instasolve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
