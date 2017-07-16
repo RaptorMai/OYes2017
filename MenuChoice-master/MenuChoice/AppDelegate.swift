b78fd98435a039d0600e648831810ed61e834872
Sulagshan <Sulagshan@gmail.com>
2017-07-16 15:16:11 -0400

    Categorization

---
+++ MenuChoice-master/MenuChoice/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  MenuChoice
+//
+//  Created by juanmao on 16/2/16.
+//  Copyright © 2016年 juanmao. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    public var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame:UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        let navC = UINavigationController(rootViewController: ViewController())
+        self.window?.rootViewController = navC
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+
+}
+
---
+++ MenuChoice-master/MenuChoice/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  MenuChoice
+//
+//  Created by juanmao on 16/2/16.
+//  Copyright © 2016年 juanmao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var productTypeArr:[String] = []
+    var productNameArr:[AnyObject] = []
+    var classifyTable: GroupTableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "MAGIC"
+        self.view.backgroundColor = UIColor.white
+        self.initData()
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+//        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+        
+        if classifyTable?.categoryselected == true {
+            print("hi")
+        }
+    }
+
+    
+    
+    func  initData()
+    {
+        let path:String = (Bundle.main.path(forResource: "MenuData", ofType: "json"))!
+        let data:Data = try! Data(contentsOf: URL(fileURLWithPath: path))
+        let json:AnyObject = try!JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as AnyObject
+        let resultDict = json.object(forKey: "data") as! Dictionary<String,AnyObject>
+        let productMenuArr:[AnyObject] = resultDict["productType"] as! Array
+        for i:Int in 0 ..< productMenuArr.count
+        {
+            productTypeArr.append(productMenuArr[i]["typeName"] as! String)
+            productNameArr.append(productMenuArr[i]["productName"] as! [String] as AnyObject)
+        }
+        
+        self.addSubView()
+        
+        
+    }
+
+    
+    func addSubView(){
+            ///调用时传入frame和数据源
+        classifyTable = GroupTableView(frame: CGRect(x: 0,y: 64,width: screenWidth,height: screenHeight-64), MenuTypeArr: productTypeArr, proNameArr: productNameArr)
+        self.view.addSubview(classifyTable!)
+    }
+
+}
+
