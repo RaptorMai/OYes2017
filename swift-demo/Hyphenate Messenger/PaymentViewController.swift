3a6205894176c48cf1e80a3eb9d0c3131cdf90dd
Ming Yue <mingyue312@gmail.com>
2017-07-26 14:31:27 -0400

    payment MVP done

--- swift-demo/Hyphenate Messenger/PaymentViewController.swift
+++ swift-demo/Hyphenate Messenger/PaymentViewController.swift
@@ -21,17 +21,19 @@ class PaymentViewController: UIViewController, STPPaymentCardTextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        var paymentInfoLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width - 30, height: 88))
-        paymentInfoLabel.center = CGPoint(x: 15, y: 0.191 * self.view.frame.height)
-        paymentInfoLabel.text = "Please enter card information and press Buy to confirm your purchase of:\n " + product + "for $\(self.price/100).00."
-        paymentInfoLabel.adjustsFontSizeToFitWidth = true
-        paymentInfoLabel.numberOfLines = 2
-        view.addSubview(paymentInfoLabel)
-        paymentTextField.frame = CGRect(x: 15, y: 0.382 * self.view.frame.height, width: self.view.frame.width - 30, height: 44)
+        paymentTextField.frame = CGRect(x: 15, y: 0.35 * self.view.frame.height, width: self.view.frame.width - 30, height: 44)
         paymentTextField.delegate = self
         view.addSubview(paymentTextField)
         payButtonOutlet.isHidden = true;
+        
+        let paymentInfoLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width - 30, height: 800))
+        paymentInfoLabel.center = CGPoint(x: self.view.frame.width/2, y: 0.191 * self.view.frame.height)
+        paymentInfoLabel.text = "Please enter card information to confirm your purchase:\n \"" + product + "\" for $\(self.price/100).00."
+        paymentInfoLabel.adjustsFontSizeToFitWidth = true
+        paymentInfoLabel.numberOfLines = 2
+        //paymentInfoLabel.layer.borderColor = (UIColor.black as! CGColor)
+        //paymentInfoLabel.layer.borderWidth = 2
+        view.addSubview(paymentInfoLabel)
     }
     
     func paymentCardTextFieldDidChange(_ textField: STPPaymentCardTextField) {
