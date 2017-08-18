1403fe6c17b419d82b370dd8ac6a2c072a915de0
RaptorMai <zhedamai0126@gmail.com>
2017-08-18 15:48:23 -0400

    Update tutor app
    1. Add spinner when press fresh
    2. Fixed bug for transaction block
    3. Update index.js: move question when end time is updated.
    4. Update quesiton list: only show question with status 0

--- Hyphenate-Demo-Swift-master/Hyphenate-Demo-Swift/Code/Controllers/Questions/QuestionsVC.swift
+++ Hyphenate-Demo-Swift-master/Hyphenate-Demo-Swift/Code/Controllers/Questions/QuestionsVC.swift
@@ -8,20 +8,42 @@
 
 import UIKit
 import Foundation
+import MBProgressHUD
 
-class QuestionsVC: UIViewController {
+
+protocol refreshSpinnerProtocol {
+    func removeSpinner()
+}
+
+
+class QuestionsVC: UIViewController, refreshSpinnerProtocol{
     
     @IBAction func refresh(_ sender: UIBarButtonItem) {
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "refresh"), object: nil)
+        MBProgressHUD.showAdded(to: tableofquestions, animated: true)
     }
     
     
     @IBOutlet weak var tableofquestions: UIView!
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let segueName = segue.identifier
+        if segueName == "magic"{
+            let childVC = segue.destination as? MainTableViewController
+            childVC?.delegate = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+    }
+    
+    func removeSpinner(){
+    
+        MBProgressHUD.hideAllHUDs(for: tableofquestions, animated: true)
+    
     }
     
 }
