4e34a692b3947790de1c077d9d0236b8ac1db721
zhan1970 <jiayun.zhang@mail.utoronto.ca>
2017-09-25 23:08:48 -0400

    Fixed 2 problems: 1.When the button help is clicked, the setting button in navigation bar changed to white; 2.When the button help is clicked, the tabbar can be dismissed and will come back if go back to setting. Furthermore, I add HUD when click help and the page is loading.

--- Hyphenate-Demo-Swift-master/Hyphenate-Demo-Swift/Code/Controllers/Settings/SettingsVC.swift
+++ Hyphenate-Demo-Swift-master/Hyphenate-Demo-Swift/Code/Controllers/Settings/SettingsVC.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 import Firebase
+import MBProgressHUD
+import MessageUI
 
-class SettingsVC: UITableViewController {
+class SettingsVC: UITableViewController, MFMailComposeViewControllerDelegate{
 /*
     @IBAction func LogOut(_ sender: Any) {
         try! Auth.auth().signOut()
@@ -30,6 +32,8 @@ class SettingsVC: UITableViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         tableView.register(UINib(nibName: "SwitchTableViewCell", bundle: nil), forCellReuseIdentifier: "switchCell")
         self.tableView.register(UINib(nibName: "LabelTableViewCell", bundle: nil), forCellReuseIdentifier: "labelCell")
+        
+        self.tabBarController?.tabBar.isHidden = false
         // Do any additional setup after loading the view.
     }
 
@@ -82,11 +86,17 @@ class SettingsVC: UITableViewController {
                 let openWebPageVC = OpenUrlViewController()
                 openWebPageVC.url = "https://www.instasolve.ca/"
                 navigationController?.pushViewController(openWebPageVC, animated: true)
+                self.tabBarController?.tabBar.isHidden = true
+                self.navigationController?.navigationBar.tintColor = UIColor.white
             case 1:
-                let settingFeedbackVC = SendFeedbackController()
-                //navigationController?.popViewController(animated: true)
-                //navigationController?.pushViewController(settingFeedbackVC, animated: true)
-                self.present(settingFeedbackVC, animated: true, completion: nil)
+                if !MFMailComposeViewController.canSendMail(){
+                    print("Mail services are not available")
+                    self.showSendMailErrorAlert()
+                    return
+                } else {
+                    sendFeedback()
+                    
+                }
                 tableView.deselectRow(at: indexPath, animated: true)
             default:break
             }
@@ -137,6 +147,63 @@ class SettingsVC: UITableViewController {
     }
     */
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK - Functions for email sending. (Feedback button)
+    
+    // send Feedback
+    func sendFeedback(){
+        let composeVC = MFMailComposeViewController()
+        composeVC.mailComposeDelegate = self
+        // Configure the fields of the interface
+        composeVC.setToRecipients(["instasolve1@gmail.com"])
+        composeVC.setSubject("Feedback - InstaSolve")
+        composeVC.setMessageBody("Please leave us your precious feedback!", isHTML: false)
+        self.present(composeVC, animated: true, completion:nil)
+        
+    }
+    
+    // error handler
+    func showSendMailErrorAlert(){
+        let sendMailErrorAlert = UIAlertController(title: "Mail cannot be sent", message: "Mailbox is not setup properly", preferredStyle: .alert )
+        sendMailErrorAlert.addAction(UIAlertAction(title: "Yes", style: .default) {_ in})
+        self.present(sendMailErrorAlert, animated: true)
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["someone@somewhere.com"])
+        mailComposerVC.setSubject("Sending you an in-app e-mail...")
+        mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    // dimiss controller
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        //Dismiss the black view controller.
+        //self.presentingViewController?.dismiss(animated: false, completion: nil)
+        
+        switch result {
+        case MFMailComposeResult.cancelled:
+            print("Mail cancelled")
+        case MFMailComposeResult.saved:
+            print("Mail saved")
+        case MFMailComposeResult.sent:
+            print("Mail sent")
+        case MFMailComposeResult.failed:
+            print("Mail sent failure")
+        default:
+            break
+        }
+        // Dismiss mail view controller and back to setting page
+        self.dismiss(animated:true, completion: nil)
+    }
 
 
 }
--- Hyphenate-Demo-Swift-master/Hyphenate-Demo-Swift/Code/Controllers/Settings/webViewController.swift
+++ Hyphenate-Demo-Swift-master/Hyphenate-Demo-Swift/Code/Controllers/Settings/webViewController.swift
@@ -9,11 +9,13 @@
 import Foundation
 import UIKit
 import WebKit
+import MBProgressHUD
 class OpenUrlViewController: UIViewController, WKUIDelegate {
     
     var url : String?
     
     var webView: WKWebView!
+    
     
     override func loadView() {
         let webConfiguration = WKWebViewConfiguration()
@@ -24,7 +26,18 @@ class OpenUrlViewController: UIViewController, WKUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Add a progressHUD when loading.
+        let myHUD = MBProgressHUD.showAdded(to: self.view, animated: true)
+        self.view.addSubview(myHUD!)
+        myHUD?.isUserInteractionEnabled = false
+        // Hide HUD after 8 seconds. (Cannot detect when the loading is finish)
+        myHUD?.hide(true, afterDelay: 5)
+        
         let myURL = URL(string: url!)
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
-    }}
+        
+    }
+    
+    
+}
